db08cd6ff07ca5478ef5ed873cef3027df00ea45
rananischal15@gmail.com <rananischal15@gmail.com>
2020-10-19 16:11:50 +1100

    navbar darkmode

--- environ/restapi/products/urls.py
+++ environ/restapi/products/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
@@ -25,3 +27,6 @@ urlpatterns = [
     path('api/', include('products.api.urls')),
     
 ]
+
+
+
--- environ/restapi/restapi/settings.py
+++ environ/restapi/restapi/settings.py
@@ -130,3 +130,7 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    
+]
